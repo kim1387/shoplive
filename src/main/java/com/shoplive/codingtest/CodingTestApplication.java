15be2734d018f261dc54b2c68a38528a1933059c
kim1387 <kevink0@naver.com>
2022-10-11 15:18:27 +0900

    chore: Initial commit

---
+++ src/main/java/com/shoplive/codingtest/CodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.shoplive.codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoplive/codingtest/CodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoplive.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
