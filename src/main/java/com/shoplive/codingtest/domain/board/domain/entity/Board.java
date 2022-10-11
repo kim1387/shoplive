8f2bb1364e63b0a0ef9362e222c1767506be5197
kim1387 <kevink0@naver.com>
2022-10-12 02:02:43 +0900

    feat: add 게시판과 작성자, 게시판과 이미지 연관 관계

--- src/main/java/com/shoplive/codingtest/domain/board/domain/entity/Board.java
+++ src/main/java/com/shoplive/codingtest/domain/board/domain/entity/Board.java
@@ -1,5 +1,7 @@
 package com.shoplive.codingtest.domain.board.domain.entity;
 
+import com.shoplive.codingtest.domain.image.domain.entity.Image;
+import com.shoplive.codingtest.domain.user.domain.entity.User;
 import com.shoplive.codingtest.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -7,24 +9,39 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "board")
 public class Board extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  // 게시판 제목
   @Column(name = "title", nullable = false)
   private String title;
 
+  // 게시판 내용
   @Lob
   @Column(name = "content", nullable = false)
   private String content;
 
+  // 게시판 삭제 유무
   @Column(name = "is_removed", nullable = false)
   private boolean isRemoved;
+
+  // 게시글 작성자
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  // 게시물과 이미지 연관관계
+  @OneToMany(mappedBy = "board")
+  private List<Image> boardImages = new ArrayList<>();
 }
