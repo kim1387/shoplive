8591cfc46290af6ee66dd3a420bffc2093f09ab7
kim1387 <kevink0@naver.com>
2022-10-12 17:55:33 +0900

    style: code formatting

--- src/main/java/com/shoplive/codingtest/global/dto/code/ErrorCode.java
+++ src/main/java/com/shoplive/codingtest/global/dto/code/ErrorCode.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    //Global
-    INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
+  // Global
+  INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
 
-    // USER
-    USER_NOT_FOUND(400, "U001", "유저를 찾을 수없습니다."),
-    ;
+  // USER
+  USER_NOT_FOUND(400, "U001", "유저를 찾을 수없습니다."),
+  ;
 
-    private final int status;
-    private final String code;
-    private final String message;
+  private final int status;
+  private final String code;
+  private final String message;
 }
