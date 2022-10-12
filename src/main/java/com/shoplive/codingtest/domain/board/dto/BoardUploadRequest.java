2ad39b5977c9763c182a2b7cfc9f94bfb7e7a314
kim1387 <kevink0@naver.com>
2022-10-13 01:34:33 +0900

    #13 feat: 게시물 생성시에 이미지 업로드 개수 2개로 제한

--- src/main/java/com/shoplive/codingtest/domain/board/dto/BoardUploadRequest.java
+++ src/main/java/com/shoplive/codingtest/domain/board/dto/BoardUploadRequest.java
@@ -3,20 +3,25 @@ package com.shoplive.codingtest.domain.board.dto;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class BoardUploadRequest {
-    // 게시판 제목
-    private String title;
 
-    // 게시글 작성자
-    private Long userId;
+  // 게시판 제목
+  @Size(max = 50, message = "게시물 제목은 50자까지 가능")
+  private String title;
 
-    // 게시판 내용
-    private String content;
+  // 게시글 작성자
+  private Long userId;
 
-    // 게시물에 함께 등록된 이미지들
-    private List<MultipartFile> boardImages = new ArrayList<>();
+  // 게시판 내용
+  @Size(max = 2000, message = "게시물 내용은 2000자까지 가능")
+  private String content;
+
+  // 게시물에 함께 등록된 이미지들
+  @Size(max = 2, message = "게시물 이미지는 2개까지 추가 가능.")
+  private List<MultipartFile> boardImages = new ArrayList<>();
 }
