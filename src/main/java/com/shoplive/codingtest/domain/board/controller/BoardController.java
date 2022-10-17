feab38f788284f32ebda341cf9f5fe14925f9ca4
kim1387 <kevink0@naver.com>
2022-10-17 12:47:59 +0900

    #17 feat: add board controller

--- src/main/java/com/shoplive/codingtest/domain/board/controller/BoardController.java
+++ src/main/java/com/shoplive/codingtest/domain/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.shoplive.codingtest.domain.board.controller;
 
 import com.shoplive.codingtest.domain.board.dto.BoardDetailResponse;
+import com.shoplive.codingtest.domain.board.dto.BoardUpdateRequest;
 import com.shoplive.codingtest.domain.board.dto.BoardUploadRequest;
 import com.shoplive.codingtest.domain.board.service.BoardService;
 import com.shoplive.codingtest.global.dto.ResultResponse;
@@ -8,6 +9,8 @@ import com.shoplive.codingtest.global.dto.code.ResultCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequestMapping("/api/v1/board")
 @RequiredArgsConstructor
@@ -17,7 +20,7 @@ public class BoardController {
   private final BoardService boardService;
 
   @PostMapping("/upload")
-  public ResponseEntity<ResultResponse> uploadImage(BoardUploadRequest request) {
+  public ResponseEntity<ResultResponse> uploadImage(@Valid BoardUploadRequest request) {
 
     BoardDetailResponse boardDetailResponse = boardService.upload(request);
     return ResponseEntity.ok(
@@ -29,5 +32,12 @@ public class BoardController {
     BoardDetailResponse response = boardService.getBoardDetail(boardId);
 
     return ResponseEntity.ok(ResultResponse.of(ResultCode.BOARD_DETAIL_GET_SUCCESS, response));
+  }
+
+  @PutMapping
+  public ResponseEntity<ResultResponse> updateBoardDetail(@Valid BoardUpdateRequest request) {
+    BoardDetailResponse response = boardService.updateBoard(request);
+
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.BOARD_DETAIL_UPDATE_SUCCESS, response));
   }
 }
